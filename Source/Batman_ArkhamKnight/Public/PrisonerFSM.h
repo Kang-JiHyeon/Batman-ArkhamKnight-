7710e9463d974a5940f9a83dea9fdf206bb9f6b3
yeanha <143509984+yeanha@users.noreply.github.com>
2024-08-05 10:12:37 +0900

    값 수치 조정

--- Source/Batman_ArkhamKnight/Public/PrisonerFSM.h
+++ Source/Batman_ArkhamKnight/Public/PrisonerFSM.h
@@ -78,10 +78,10 @@ public:
 
 	// move or idle
 	UPROPERTY(EditAnywhere,Category= PFSM)
-	float moveDelayTime = 5;
+	float moveDelayTime = 4;
 
 	UPROPERTY(EditAnywhere,Category= PFSM)
-	float backmoveDelayTime = 6;
+	float backmoveDelayTime = 5;
 
 	// attack
 	UPROPERTY(EditAnywhere,Category= PFSM)
@@ -97,7 +97,7 @@ public:
 
 	// faint
 	UPROPERTY(EditAnywhere,Category= PFSM)
-	float FaintDelayTime = 10.0f;
+	float FaintDelayTime = 8.0f;
 
 	// HP
 	
