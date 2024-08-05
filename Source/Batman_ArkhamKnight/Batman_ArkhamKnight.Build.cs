e27841e1a392b2e366141dd477d76da51a3022df
hurgayoung <charmant.rachel@gmail.com>
2024-08-05 19:51:42 +0900

    Plugin, Module 설정

--- Source/Batman_ArkhamKnight/Batman_ArkhamKnight.Build.cs
+++ Source/Batman_ArkhamKnight/Batman_ArkhamKnight.Build.cs
@@ -7,10 +7,10 @@ public class Batman_ArkhamKnight : ModuleRules
 	public Batman_ArkhamKnight(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "LevelSequence", "MovieScene", "MotionWarping", "UMG", "Niagara", "ChaosVehicles" });
+
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
